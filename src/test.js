01aab4c33e29c8ebfab19af968d8139aeb5fd99b
mvjdev <hei.maria.625@gmail.com>
2025-05-16 18:45:06 +0300

    fix: workflows

--- src/test.js
+++ src/test.js
@@ -1,4 +1,3 @@
 "use strict";
-// src/index.ts
 const message = "Hello TypeScript!";
 console.log(message);
--- src/test.ts
+++ src/test.ts
@@ -1,3 +1,2 @@
-// src/index.ts
 const message: string = "Hello TypeScript!";
 console.log(message);
