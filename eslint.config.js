8d84eed7710d6aaa994ce4d644af18dc5c2343e1
mvjdev <hei.maria.625@gmail.com>
2025-05-16 18:50:42 +0300

    fix: workflows

---
+++ eslint.config.js
@@ -0,0 +1,20 @@
+// eslint.config.js
+import eslintPluginTs from '@typescript-eslint/eslint-plugin'
+import parserTs from '@typescript-eslint/parser'
+
+export default [
+  {
+    files: ['**/*.ts'],
+    languageOptions: {
+      parser: parserTs,
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+    },
+    plugins: {
+      '@typescript-eslint': eslintPluginTs,
+    },
+    rules: {
+        
+    },
+  },
+]
